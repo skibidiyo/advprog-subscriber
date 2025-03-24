38b887403bdea2935101147612a734d4e39f7ac5
skibidiyo <diyobanu@gmail.com>
2025-03-24 11:25:36 +0700

    Implement list_messages function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -96,4 +96,8 @@ impl NotificationService {
         let subscriber_request: Notification = NotificationRepository::add(payload);
         return Ok(subscriber_request);
     }
+
+    pub fn list_messages() -> Result<Vec<String>> {
+        return Ok(NotificationRepository::list_all_as_string());
+    }
 }
